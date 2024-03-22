36976537a64c0cd93e7e38d34e702f44afaf9c87
abigailojeda <abigail.arte@gmail.com>
2024-03-22 16:02:24 +0000

    Intro scene

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class IntroController : MonoBehaviour {
+    [SerializeField] GameObject[] texts;
+    int index = 0;
+
+    void Start() {
+        texts[index].SetActive(true);
+    }
+
+    public void ShowNext() {
+        texts[index].SetActive(false);
+        if (index == 2) index = 0;
+        texts[++index].SetActive(true);
+    }
+
+    public void ShowPrev() {
+        texts[index].SetActive(false);
+        if (index == 0) index = 2;
+        texts[--index].SetActive(true);
+    }
+}
