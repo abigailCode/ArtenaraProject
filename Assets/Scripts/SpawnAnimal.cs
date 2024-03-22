49e29b18f3ecc54ed22b6bd8d0066eab3cf4c6b9
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-03-22 18:34:43 +0000

    last review

--- Assets/Scripts/SpawnAnimal.cs
+++ Assets/Scripts/SpawnAnimal.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class SpawnAnimal : MonoBehaviour
 {
     [SerializeField] private GameObject[] animalPrefabs;
+    [SerializeField] private GameObject spawnPoint;
 
     public void Spawn()
     {
         int randomIndex = Random.Range(0, animalPrefabs.Length);
-        Instantiate(animalPrefabs[randomIndex], transform.position, Quaternion.identity);
+        Instantiate(animalPrefabs[randomIndex], spawnPoint.transform.position, Quaternion.identity);
     }
 }
