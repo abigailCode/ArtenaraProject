d3b5a6d9ef0a6891f242137ca0abbc6621093c31
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-04-19 19:25:35 +0100

    AutoFollowMe

--- Assets/Scripts/GestureHandler.cs
+++ Assets/Scripts/GestureHandler.cs
@@ -6,43 +6,89 @@ using UnityEngine;
 
 public class GestureHandler : MonoBehaviour, IMixedRealityGestureHandler<Vector3>
 {
-    void OnEnable()
+
+    [SerializeField] GameObject prefabToInstantiate;
+    [SerializeField] MixedRealityInputAction actionToInstantiate = MixedRealityInputAction.None;
+    [SerializeField] Transform mixedRealitySceneContent;
+    GameObject instancedPrefabCopy;
+    Vector3 initialPrefabCopyPosition;
+
+    public void OnEnable()
     {
         CoreServices.InputSystem?.RegisterHandler<IMixedRealityGestureHandler<Vector3>>(this);
     }
 
+    public void OnDisable()
+    {
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityGestureHandler<Vector3>>(this);
+    }
+
+    public void DestroyInstancedPrefab()
+    {
+        Debug.Log("PLEASE WORK");
+        if (instancedPrefabCopy != null)
+        {
+            Destroy(instancedPrefabCopy);
+            instancedPrefabCopy = null;
+        }
+    }
+
+    public void OnGestureCanceled(InputEventData eventData)
+    {
+        Debug.Log("PLEASE WORK");
+        if (actionToInstantiate == eventData.MixedRealityInputAction)
+        {
+            DestroyInstancedPrefab();
+        }
+    }
+
     public void OnGestureCompleted(InputEventData<Vector3> eventData)
     {
-
+        Debug.Log("PLEASE WORK");
+        if (actionToInstantiate == eventData.MixedRealityInputAction)
+        {
+            DestroyInstancedPrefab();
+        }
     }
 
-    void IMixedRealityGestureHandler<Vector3>.OnGestureUpdated(InputEventData<Vector3> eventData)
+    public void OnGestureCompleted(InputEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("PLEASE WORK");
+        if (actionToInstantiate == eventData.MixedRealityInputAction)
+        {
+            DestroyInstancedPrefab();
+        }
     }
 
-    void IMixedRealityGestureHandler<Vector3>.OnGestureCompleted(InputEventData<Vector3> eventData)
+    public void OnGestureStarted(InputEventData<Vector3> eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("PLEASE WORK");
+        if (actionToInstantiate == eventData.MixedRealityInputAction && prefabToInstantiate != null)
+        {
+            instancedPrefabCopy = Instantiate(prefabToInstantiate);
+            instancedPrefabCopy.transform.parent = mixedRealitySceneContent;
+            Vector3 newPosition = Camera.main.transform.position + Camera.main.transform.forward * 2.0f;
+            instancedPrefabCopy.transform.position = newPosition;
+            initialPrefabCopyPosition = newPosition;
+        }
     }
 
-    void IMixedRealityGestureHandler.OnGestureStarted(InputEventData eventData)
+    public void OnGestureStarted(InputEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("PLEASE WORK");
     }
 
-    void IMixedRealityGestureHandler.OnGestureUpdated(InputEventData eventData)
+    public void OnGestureUpdated(InputEventData<Vector3> eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("PLEASE WORK");
+        if (actionToInstantiate == eventData.MixedRealityInputAction && instancedPrefabCopy != null)
+        {
+            instancedPrefabCopy.transform.position = initialPrefabCopyPosition + eventData.InputData;
+        }
     }
 
-    void IMixedRealityGestureHandler.OnGestureCompleted(InputEventData eventData)
+    public void OnGestureUpdated(InputEventData eventData)
     {
-        throw new System.NotImplementedException();
-    }
-
-    void IMixedRealityGestureHandler.OnGestureCanceled(InputEventData eventData)
-    {
-        throw new System.NotImplementedException();
+        Debug.Log("PLEASE WORK");
     }
 }
